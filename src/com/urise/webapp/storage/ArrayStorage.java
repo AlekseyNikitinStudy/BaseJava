35c5f95a30e7ca862df0d235c431ea920327c844
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-09-23 22:05:43 +0400

    updates of lesson2

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -10,56 +10,57 @@ import java.util.Arrays;
 public class ArrayStorage {
     private Resume[] storage = new Resume[10_000];
     private int size;
+    private int index;
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    public void save(Resume r) {
+    public void save(Resume resume) {
         if (size < storage.length) {
-            if (get(r.getUuid()) == null) {
-                storage[size] = r;
+            if (getIndex(resume.getUuid()) < 0) {
+                storage[size] = resume;
                 size++;
             } else {
-                System.out.println("Резюме с таким UUID есть в ArrayStorage.");
+                System.out.println("Резюме с UUID = " + resume.getUuid() + " есть в ArrayStorage.");
             }
         } else {
             System.out.println("Переполнение ArrayStorage.");
         }
     }
 
-    public void update(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(r.getUuid())) {
-                storage[i] = r;
-                return;
-            }
+    public void update(Resume resume) {
+        if ((index = getIndex(resume.getUuid())) >= 0) {
+            storage[index] = resume;
         }
-        System.out.println("Нет Резюме с таким UUID в ArrayStorage.");
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
+        if ((index = getIndex(uuid)) >= 0) {
+            return storage[index];
+        } else {
+            return null;
         }
-        System.out.println("Нет Резюме с таким UUID в ArrayStorage.");
-        return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                if (size - i + 1 >= 0) {
-                    System.arraycopy(storage, i + 1, storage, i, size - i + 1);
-                    size--;
-                    return;
-                }
+        if ((index = getIndex(uuid)) >= 0) {
+            if (size - index + 1 >= 0) {
+                System.arraycopy(storage, index + 1, storage, index, size - index + 1);
+                size--;
             }
         }
-        System.out.println("Нет Резюме с таким UUID в ArrayStorage.");
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        System.out.println("Нет Резюме с UUID = " + uuid + " в ArrayStorage.");
+        return -1;
     }
 
     /**
