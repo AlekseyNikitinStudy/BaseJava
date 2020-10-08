5bff25c1ecdafa8b2dbbc6852ffa63400cf47ebe
Aleksey Nikitin <69401560+AlekseyNikitinStudy@users.noreply.github.com>
2020-10-08 14:47:36 +0300

    Update SortedArrayStorage.java

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    protected void addElement(int index, Resume resume){
+    void addElement(int index, Resume resume){
         int indexAdd = - index - 1;
         System.arraycopy(storage, indexAdd, storage, indexAdd + 1, size - indexAdd);
         storage[indexAdd] = resume;
     }
 
-    protected void removeElement(int index) {
+    void removeElement(int index) {
         int elementsMoved = size - index - 1;
         if (elementsMoved > 0) {
             System.arraycopy(storage, index + 1, storage, index, elementsMoved);
@@ -20,7 +20,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     }
 
     @Override
-    protected int getIndex(String uuid) {
+    int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
