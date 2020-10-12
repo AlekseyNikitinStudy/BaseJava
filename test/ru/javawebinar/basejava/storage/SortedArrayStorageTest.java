8b2d1f2afa5be2d5e903ddc0aa89310824f67fd2
AlekseyNikitinStudy <alekseynikitin2020@yandex.ru>
2020-10-12 22:37:03 +0400

    Updates of lesson4

--- test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SortedArrayStorageTest.java
@@ -5,20 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest{
-
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
-    }
-
-    @Test
-    public void addElement() {
-    }
-
-    @Test
-    public void removeElement() {
-    }
-
-    @Test
-    public void getIndex() {
     }
 }
